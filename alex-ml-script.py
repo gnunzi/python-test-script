9737674aebe33bdcc60f412220827501c822835a
digisensor <giorgio.nunzi@azeti.net>
2021-12-21 16:55:01 +0100

    second try

--- alex-ml-script.py
+++ alex-ml-script.py
@@ -6,7 +6,7 @@ my_test_file=open("alextestfile.txt","w")
 
 counter = 1
 while True:
-    print("My script writes: "+str(counter),file=my_test_file)
+    print("My new version: "+str(counter),file=my_test_file)
     my_test_file.flush()
     time.sleep(5)
     counter+=1
