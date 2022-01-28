23d47234bb4e3fff21bbc346787051c67c5fe2ab
digisensor <giorgio.nunzi@azeti.net>
2022-01-28 15:23:27 +0100

    Added exception catcg

--- hello-world.py
+++ hello-world.py
@@ -3,19 +3,21 @@ import util.az_lib as az_lib
 from util.az_logger import AzetiLogger
 logger = AzetiLogger()
 
-print("Hello world")
-teststr="test string"
-print(teststr)
+try:
+    print("Hello world")
+    teststr="test string"
+    print(teststr)
 
-my_test_file=open("testfile.txt","w")
-print(teststr,file=my_test_file)
+    my_test_file=open("testfile.txt","w")
+    print(teststr,file=my_test_file)
 
-counter = 1
-while True:
-    logger.info("Printing data with cycle: "+str(counter))
-    print(teststr+str(counter),file=my_test_file)
-    my_test_file.flush()
-    az_lib.publish("My test value",counter)
-    counter+=1
-    time.sleep(5)
-    
+    counter = 1
+    while True:
+        logger.info("Printing data with cycle: "+str(counter))
+        print(teststr+str(counter),file=my_test_file)
+        my_test_file.flush()
+        az_lib.publish("My test value",counter)
+        counter+=1
+        time.sleep(5)
+except Exception as exception:
+    logger.logTraceStackToLogFile()
