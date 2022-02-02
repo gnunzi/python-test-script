604fb81a541b3d73646953b9c3108b92c9907284
digisensor <giorgio.nunzi@azeti.net>
2022-02-02 20:58:26 +0100

    changing main script to .sh

--- hello-world.py
+++ hello-world.py
@@ -2,17 +2,21 @@ import time
 import traceback
 import util.az_lib as az_lib
 from util.az_logger import AzetiLogger
-logger = AzetiLogger()
-azLib=az_lib.AzLib()
+logger = AzetiLogger() # In next version, this could be any logger
+azLib=az_lib.AzLib() # The azimuth Execution Environment will inject the azLibEE here
 
 try:
-    print("Hello world")
+    print("This is a test script. We will create a counter and increment it every 5 seconds.")
+    print("You will see this message to the console only in your dev environment")
     teststr="test string"
-    print(teststr)
     filename="testfile.txt"
     my_test_file=open(filename,"w")
     print(teststr,file=my_test_file)
+except Exception as exception:
+    traceback.print_exc()
+    logger.logTraceStackToLogFile()
 
+def my_create_data():
     counter = 1
     while True:
         logger.info("Printing data with cycle "+str(counter)+" to file "+filename)
@@ -21,6 +25,3 @@ try:
         azLib.publish_sensor_value("My test value",counter)
         counter+=1
         time.sleep(5)
-except Exception as exception:
-    traceback.print_exc()
-    logger.logTraceStackToLogFile()
