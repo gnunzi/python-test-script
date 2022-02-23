5851abf59fdd56b95b4b066dfa5414919a4d6685
digisensor <giorgio.nunzi@azeti.net>
2022-02-23 21:11:02 +0100

    fix logger

--- create-data.py
+++ create-data.py
@@ -2,23 +2,26 @@ import time
 import traceback
 import az_lib.az_lib as az_lib
 #from util.az_logger import AzetiLogger
-#logger = AzetiLogger() # In next version, this could be any logger
-azLib=az_lib.AzLib() # The azimuth Execution Environment will inject the azLibEE here
+# logger = AzetiLogger() # In next version, this could be any logger
+# The azimuth Execution Environment will inject the azLibEE here
+azLib = az_lib.AzLib('create-data')
+
 
 def my_create_data():
     counter = 1
-    filename="testfile.txt"
-    my_test_file=open(filename,"w")
+    filename = "testfile.txt"
+    my_test_file = open(filename, "w")
     while True:
- #       logger.info("Printing data with cycle "+str(counter)+" to file "+filename)
+     #       logger.info("Printing data with cycle "+str(counter)+" to file "+filename)
         print("Printing data with cycle "+str(counter)+" to file "+filename)
-        print("My test value: "+str(counter),file=my_test_file)
+        print("My test value: "+str(counter), file=my_test_file)
         my_test_file.flush()
-        azLib.publish_sensor_value("My test value",str(counter))
-        counter+=1
-        if counter==40:
+        azLib.publish_sensor_value("My test value", str(counter))
+        counter += 1
+        if counter == 40:
             return
         time.sleep(5)
+
 
 try:
     print("This is a test script. We will create a counter and increment it every 5 seconds.")
@@ -26,5 +29,4 @@ try:
     my_create_data()
 except Exception as exception:
     traceback.print_exc()
-    #logger.logTraceStackToLogFile()
-
+    # logger.logTraceStackToLogFile()
